b9ccf1a71c2419c1610279380d61fb87533d1619
brianhanna <bhanna1693@gmail.com>
2023-08-23 15:58:03 -0400

    test changes

--- src/test/java/com/bhanna/oddsapi/OddsApiApplicationTests.java
+++ src/test/java/com/bhanna/oddsapi/OddsApiApplicationTests.java
@@ -3,11 +3,14 @@ package com.bhanna.oddsapi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class OddsApiApplicationTests {
 
     @Test
     void contextLoads() {
+        assertTrue(true);
     }
 
 }
